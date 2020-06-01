99157aa8d9cf72a2671d9b40014704b7ff2b775b
Alex Musabelliu <a.musabell@gmail.com>
2020-05-31 22:16:53 -0400

    Add files via upload

---
+++ cue.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+if len(sys.argv) < 2:
+    loc = input("Folder dir: ")
+else:
+    loc = sys.argv[1]
+    
+os.chdir(os.path.abspath(loc))
+
+music = os.listdir(loc)
+
+with open("{0}.cue".format(loc[loc.rfind('\\') + 1:]), "w") as f:
+    for i in range(len(music)):
+        song = music[i]
+        end = song[song.rfind(".") + 1:]
+
+        translation = {
+            "flac":"WAVE",
+            "mp3":"MP3",
+            "wav":"WAVE"
+        }
+
+        end = translation.get(end)
+        f.write(f"FILE \"{song}\" {end}\n  TRACK {'0' * max(0, 2 - len(str(i + 1))) + str(i + 1)} AUDIO\n    INDEX 01 00:00:00\n")
