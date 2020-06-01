1eca555fb97d209941192cc4e39f42e7eb9ea44b
Alex Musabelliu <a.musabell@gmail.com>
2020-05-31 22:19:14 -0400

    Add files via upload

--- cue.py
+++ cue.py
@@ -21,5 +21,6 @@ with open("{0}.cue".format(loc[loc.rfind('\\') + 1:]), "w") as f:
             "wav":"WAVE"
         }
 
-        end = translation.get(end)
+        end = translation.get(end, "WAVE")
+        
         f.write(f"FILE \"{song}\" {end}\n  TRACK {'0' * max(0, 2 - len(str(i + 1))) + str(i + 1)} AUDIO\n    INDEX 01 00:00:00\n")
